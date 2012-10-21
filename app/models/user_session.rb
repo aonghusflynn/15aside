3b835dd09a5fa0cee910bd2ac935913fb31908f6
Aonghus Flynn <developer@aonghusflynn.com>
2012-10-21 22:03:07 +0100

    authlogic setup
    Followed
    http://www.logansbailey.com/2010/10/06/how-to-setup-authlogic-in-rails-3
    /

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -1,5 +1,5 @@
 class UserSession  < Authlogic::Session::Base
-   attr_accessible :login, :password, :remember_me
+   
    
    def to_key
       new_record? ? nil : [ self.send(self.class.primary_key) ]
